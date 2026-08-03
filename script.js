a47ad85fee8545e6c38b412c91ff2f008c5cdd80
Cesar <cesar3130of@gmail.com>
2026-08-02 21:38:41 -0400

    Implementacion de confirmacion de cita mediante js

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 const modal = document.getElementById("modal");
 const abrir = document.getElementById("abrirModal");
+const cerrar = document.querySelector(".cerrar");
+
 const boton = document.getElementById("menuBtn");
 const menu = document.getElementById("menu");
+
+const formulario = document.getElementById("formularioCita");
+
+const modalConfirmacion = document.getElementById("modalConfirmacion");
+const cerrarConfirmacion = document.getElementById("cerrarConfirmacion");
 
 abrir.addEventListener("click", function(e){
     e.preventDefault();
     modal.style.display = "flex";
 });
 
-//Evento que al darle click al fondo oscuro, el modal (Formulario) desaparece.
+cerrar.addEventListener("click", function(){
+    modal.style.display = "none";
+});
+
 window.addEventListener("click", function(e){
     if(e.target === modal){
         modal.style.display = "none";
     }
 });
 
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();
+    modal.style.display = "none";
+    modalConfirmacion.style.display = "flex";
+});
 
-boton.addEventListener("click", function(){
+cerrarConfirmacion.addEventListener("click", function(){
+    modalConfirmacion.style.display = "none";
+});
+
+boton.addEventListener("click", function () {
     menu.classList.toggle("activo");
 });
